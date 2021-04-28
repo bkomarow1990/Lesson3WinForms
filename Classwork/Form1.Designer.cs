755784602498ff725bc3eb1809eb84042f72972b
Bogdan <71147719+bkomarow1990@users.noreply.github.com>
2021-04-28 13:47:03 +0300

    Paint HW

--- Classwork/Form1.Designer.cs
+++ Classwork/Form1.Designer.cs
@@ -34,26 +34,38 @@ namespace Classwork
             this.blackbtn = new System.Windows.Forms.Button();
             this.yellowbtn = new System.Windows.Forms.Button();
             this.bluebtn = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.clearbtn = new System.Windows.Forms.Button();
+            this.stamp1Button = new System.Windows.Forms.Button();
+            this.stamp2Button = new System.Windows.Forms.Button();
+            this.thicknessLbl = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.rectBtn = new System.Windows.Forms.Button();
+            this.penBtn = new System.Windows.Forms.Button();
+            this.stampsLbl = new System.Windows.Forms.Label();
+            this.toolsLbl = new System.Windows.Forms.Label();
+            this.colorsLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.panel1.Location = new System.Drawing.Point(253, 27);
+            this.panel1.Location = new System.Drawing.Point(289, 36);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(614, 424);
+            this.panel1.Size = new System.Drawing.Size(702, 565);
             this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             // 
             // redbtn
             // 
             this.redbtn.BackColor = System.Drawing.Color.Red;
-            this.redbtn.Location = new System.Drawing.Point(12, 12);
+            this.redbtn.Location = new System.Drawing.Point(14, 16);
+            this.redbtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.redbtn.Name = "redbtn";
-            this.redbtn.Size = new System.Drawing.Size(72, 67);
+            this.redbtn.Size = new System.Drawing.Size(82, 89);
             this.redbtn.TabIndex = 0;
             this.redbtn.UseVisualStyleBackColor = false;
             this.redbtn.Click += new System.EventHandler(this.redbtn_Click);
@@ -61,9 +73,10 @@ namespace Classwork
             // blackbtn
             // 
             this.blackbtn.BackColor = System.Drawing.Color.Black;
-            this.blackbtn.Location = new System.Drawing.Point(137, 12);
+            this.blackbtn.Location = new System.Drawing.Point(157, 16);
+            this.blackbtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.blackbtn.Name = "blackbtn";
-            this.blackbtn.Size = new System.Drawing.Size(72, 67);
+            this.blackbtn.Size = new System.Drawing.Size(82, 89);
             this.blackbtn.TabIndex = 1;
             this.blackbtn.UseVisualStyleBackColor = false;
             this.blackbtn.Click += new System.EventHandler(this.blackbtn_Click);
@@ -71,9 +84,10 @@ namespace Classwork
             // yellowbtn
             // 
             this.yellowbtn.BackColor = System.Drawing.Color.Yellow;
-            this.yellowbtn.Location = new System.Drawing.Point(137, 103);
+            this.yellowbtn.Location = new System.Drawing.Point(157, 137);
+            this.yellowbtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.yellowbtn.Name = "yellowbtn";
-            this.yellowbtn.Size = new System.Drawing.Size(72, 67);
+            this.yellowbtn.Size = new System.Drawing.Size(82, 89);
             this.yellowbtn.TabIndex = 3;
             this.yellowbtn.UseVisualStyleBackColor = false;
             this.yellowbtn.Click += new System.EventHandler(this.yellowbtn_Click);
@@ -81,61 +95,153 @@ namespace Classwork
             // bluebtn
             // 
             this.bluebtn.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.bluebtn.Location = new System.Drawing.Point(12, 103);
+            this.bluebtn.Location = new System.Drawing.Point(14, 137);
+            this.bluebtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.bluebtn.Name = "bluebtn";
-            this.bluebtn.Size = new System.Drawing.Size(72, 67);
+            this.bluebtn.Size = new System.Drawing.Size(82, 89);
             this.bluebtn.TabIndex = 2;
             this.bluebtn.UseVisualStyleBackColor = false;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10"});
-            this.comboBox1.Location = new System.Drawing.Point(45, 201);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(138, 23);
-            this.comboBox1.TabIndex = 4;
-            this.comboBox1.Text = "10";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            this.bluebtn.Click += new System.EventHandler(this.bluebtn_Click);
             // 
             // clearbtn
             // 
             this.clearbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.clearbtn.Location = new System.Drawing.Point(75, 230);
+            this.clearbtn.Location = new System.Drawing.Point(86, 307);
+            this.clearbtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.clearbtn.Name = "clearbtn";
-            this.clearbtn.Size = new System.Drawing.Size(75, 23);
+            this.clearbtn.Size = new System.Drawing.Size(86, 31);
             this.clearbtn.TabIndex = 0;
             this.clearbtn.Text = "CLEAR";
             this.clearbtn.UseVisualStyleBackColor = true;
             this.clearbtn.Click += new System.EventHandler(this.clearbtn_Click);
             // 
+            // stamp1Button
+            // 
+            this.stamp1Button.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.stamp1Button.BackgroundImage = global::Classwork.Properties.Resources._1;
+            this.stamp1Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stamp1Button.Location = new System.Drawing.Point(14, 359);
+            this.stamp1Button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.stamp1Button.Name = "stamp1Button";
+            this.stamp1Button.Size = new System.Drawing.Size(82, 89);
+            this.stamp1Button.TabIndex = 5;
+            this.stamp1Button.UseVisualStyleBackColor = false;
+            this.stamp1Button.Click += new System.EventHandler(this.stamp1Button_Click);
+            // 
+            // stamp2Button
+            // 
+            this.stamp2Button.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.stamp2Button.BackgroundImage = global::Classwork.Properties.Resources._2;
+            this.stamp2Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stamp2Button.Location = new System.Drawing.Point(157, 359);
+            this.stamp2Button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.stamp2Button.Name = "stamp2Button";
+            this.stamp2Button.Size = new System.Drawing.Size(82, 89);
+            this.stamp2Button.TabIndex = 6;
+            this.stamp2Button.UseVisualStyleBackColor = false;
+            this.stamp2Button.Click += new System.EventHandler(this.stamp2Button_Click);
+            // 
+            // thicknessLbl
+            // 
+            this.thicknessLbl.AutoSize = true;
+            this.thicknessLbl.Location = new System.Drawing.Point(51, 244);
+            this.thicknessLbl.Name = "thicknessLbl";
+            this.thicknessLbl.Size = new System.Drawing.Size(74, 20);
+            this.thicknessLbl.TabIndex = 7;
+            this.thicknessLbl.Text = "Thickness:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(22, 273);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // rectBtn
+            // 
+            this.rectBtn.BackColor = System.Drawing.Color.Transparent;
+            this.rectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rectBtn.Font = new System.Drawing.Font("Bebas Neue Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rectBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rectBtn.Location = new System.Drawing.Point(14, 504);
+            this.rectBtn.Name = "rectBtn";
+            this.rectBtn.Size = new System.Drawing.Size(74, 77);
+            this.rectBtn.TabIndex = 0;
+            this.rectBtn.Text = "Rectangle";
+            this.rectBtn.UseVisualStyleBackColor = false;
+            this.rectBtn.Click += new System.EventHandler(this.rectBtn_Click);
+            // 
+            // penBtn
+            // 
+            this.penBtn.BackColor = System.Drawing.Color.Transparent;
+            this.penBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.penBtn.Font = new System.Drawing.Font("Bebas Neue Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.penBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.penBtn.Location = new System.Drawing.Point(157, 504);
+            this.penBtn.Name = "penBtn";
+            this.penBtn.Size = new System.Drawing.Size(74, 77);
+            this.penBtn.TabIndex = 8;
+            this.penBtn.Text = "PEN";
+            this.penBtn.UseVisualStyleBackColor = false;
+            this.penBtn.Click += new System.EventHandler(this.penBtn_Click);
+            // 
+            // stampsLbl
+            // 
+            this.stampsLbl.AutoSize = true;
+            this.stampsLbl.Location = new System.Drawing.Point(19, 335);
+            this.stampsLbl.Name = "stampsLbl";
+            this.stampsLbl.Size = new System.Drawing.Size(61, 20);
+            this.stampsLbl.TabIndex = 9;
+            this.stampsLbl.Text = "Stamps:";
+            // 
+            // toolsLbl
+            // 
+            this.toolsLbl.AutoSize = true;
+            this.toolsLbl.Location = new System.Drawing.Point(19, 481);
+            this.toolsLbl.Name = "toolsLbl";
+            this.toolsLbl.Size = new System.Drawing.Size(47, 20);
+            this.toolsLbl.TabIndex = 10;
+            this.toolsLbl.Text = "Tools:";
+            // 
+            // colorsLbl
+            // 
+            this.colorsLbl.AutoSize = true;
+            this.colorsLbl.Location = new System.Drawing.Point(97, 109);
+            this.colorsLbl.Name = "colorsLbl";
+            this.colorsLbl.Size = new System.Drawing.Size(54, 20);
+            this.colorsLbl.TabIndex = 11;
+            this.colorsLbl.Text = "Colors:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(902, 520);
+            this.ClientSize = new System.Drawing.Size(1019, 741);
+            this.Controls.Add(this.colorsLbl);
+            this.Controls.Add(this.toolsLbl);
+            this.Controls.Add(this.stampsLbl);
+            this.Controls.Add(this.penBtn);
+            this.Controls.Add(this.rectBtn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.thicknessLbl);
+            this.Controls.Add(this.stamp2Button);
+            this.Controls.Add(this.stamp1Button);
             this.Controls.Add(this.clearbtn);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.yellowbtn);
             this.Controls.Add(this.bluebtn);
             this.Controls.Add(this.blackbtn);
             this.Controls.Add(this.redbtn);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "YoptaPaint";
+            this.Text = "TipaPaint";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,8 +252,16 @@ namespace Classwork
         private System.Windows.Forms.Button blackbtn;
         private System.Windows.Forms.Button yellowbtn;
         private System.Windows.Forms.Button bluebtn;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button clearbtn;
+        private System.Windows.Forms.Button stamp1Button;
+        private System.Windows.Forms.Button stamp2Button;
+        private System.Windows.Forms.Label thicknessLbl;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button rectBtn;
+        private System.Windows.Forms.Button penBtn;
+        private System.Windows.Forms.Label stampsLbl;
+        private System.Windows.Forms.Label toolsLbl;
+        private System.Windows.Forms.Label colorsLbl;
     }
 }
 
--- Classwork/Form1.cs
+++ Classwork/Form1.cs
@@ -12,16 +12,23 @@ namespace Classwork
 {
     public partial class Form1 : Form
     {
+
+        Graphics graphics;
+        Image stampImage;
+        public enum Mods { Paint,Stamp, Rectangle}
+        Mods mode;
+        Point rectPoint;
         Color paintColor=Color.Black;
         int paintThickness=10;
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+             graphics = panel1.CreateGraphics();
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
@@ -50,11 +57,47 @@ namespace Classwork
         {
             if (e.Button == MouseButtons.Left)
             {
-                Label lbl = new Label();
-                lbl.BackColor = this.paintColor;
-                lbl.Size = new Size(paintThickness,paintThickness);
-                lbl.Location = e.Location;
-                this.panel1.Controls.Add(lbl);
+                if (mode == Mods.Paint)
+                {
+                    Label lbl = new Label();
+                    lbl.BackColor = this.paintColor;
+                    lbl.Size = new Size(paintThickness, paintThickness);
+                    lbl.Location = e.Location;
+                    this.panel1.Controls.Add(lbl);
+                }
+                else if (mode == Mods.Stamp)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Image = this.stampImage;
+                    //Image.FromFile("../../../Resources/1.png"); 
+                    pb.Size = new Size(paintThickness, paintThickness);
+                    pb.Location = e.Location;
+                    this.panel1.Controls.Add(pb);
+                }
+                else if(mode == Mods.Rectangle)
+                {
+                    Point p = e.Location;
+                    int x = Math.Min(rectPoint.X, p.X);
+                    int y = Math.Min(rectPoint.Y, p.Y);
+                    int width = Math.Abs(p.X - rectPoint.X);
+                    int height = Math.Abs(p.Y - rectPoint.Y);
+                    Rectangle rect = new Rectangle(rectPoint.X, rectPoint.Y, width, height);
+                    graphics.DrawRectangle(new Pen(new SolidBrush(paintColor)),rect);
+                    graphics.FillRectangle(new SolidBrush(paintColor),rect);
+                    //Rectangle rect;
+                    //graphics.FillRectangle(new SolidBrush(paintColor), rect);
+
+                    //this.panel1.Controls.Add(rect);
+                    this.Invalidate();
+                    // mRect = new Rectangle(e.X, e.Y, 0, 0);
+                    //this.Invalidate();
+
+                }
+                else
+                {
+                    MessageBox.Show("Select Tool","Info",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                
             }
             
         }
@@ -65,9 +108,45 @@ namespace Classwork
             this.panel1.Controls.Clear();
         }
 
-        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        private void bluebtn_Click(object sender, EventArgs e)
         {
-            this.paintThickness = Int32.Parse(this.comboBox1.SelectedItem.ToString());
+            this.paintColor = Color.Blue;
+        }
+
+        private void stamp1Button_Click(object sender, EventArgs e)
+        {
+            this.mode = Mods.Stamp;
+            this.stampImage = Classwork.Properties.Resources._1;
+        }
+
+        private void stamp2Button_Click(object sender, EventArgs e)
+        {
+            this.mode = Mods.Stamp;
+            this.stampImage = Classwork.Properties.Resources._2;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.paintThickness = (int)this.numericUpDown1.Value;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mode == Mods.Rectangle)
+            {
+                rectPoint = e.Location;
+            }
+            
+        }
+
+        private void rectBtn_Click(object sender, EventArgs e)
+        {
+            this.mode = Mods.Rectangle;
+        }
+
+        private void penBtn_Click(object sender, EventArgs e)
+        {
+            this.mode = Mods.Paint;
         }
     }
 }
